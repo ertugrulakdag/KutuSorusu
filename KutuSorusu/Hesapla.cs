5983e80c8a6bfb8bd2520f619e2383cc8af6a61f
ertugrulakdag <ertugrulakdag@gmail.com>
2023-10-24 00:15:41 +0300

    Add project files.

---
+++ KutuSorusu/Hesapla.cs
@@ -0,0 +1,71 @@
+﻿using Pastel;
+using System.Drawing;
+
+namespace KutuSorusu
+{
+    public class Kutu
+    {
+        public async Task<int> AlaniniHesapla(int[] kutuBoyutlari)
+        {
+            //Dizinin her elemanı için yükseklik * taban'ı hesaplarım.
+            var keyValue = new List<KeyValuePair<string, int>>();//Her elemanın sonucunu keyValue KeyValuePair'sine yazmak için hazırlık yaptım.
+            var terstenKutuBoyutlari = kutuBoyutlari.Reverse().ToArray();
+            for (int i = 0; i < kutuBoyutlari.Count(); i++)
+            {
+                int birinciKutuAlani = 0;
+                int ikinciKutuAlani = 0;
+
+                int ilgiliDiziElemani = kutuBoyutlari[i];
+
+                int[] birinciKutuDizisi = new int[i + 1];
+                int[] ikinciKutuDizisi = new int[kutuBoyutlari.Count() - (i + 1)];
+
+                for (int j = 0; j < birinciKutuDizisi.Count(); j++)
+                {
+                    birinciKutuDizisi[j] = kutuBoyutlari[j];
+                }
+                birinciKutuAlani = await BirinciKutuHesapla(birinciKutuDizisi);
+
+                for (int k = 0; k < ikinciKutuDizisi.Count(); k++)
+                {
+                    ikinciKutuDizisi[k] = terstenKutuBoyutlari[k];
+                }
+
+                ikinciKutuAlani = await IkinciKutuHesapla(ikinciKutuDizisi);
+                int alan = birinciKutuAlani + ikinciKutuAlani;
+                keyValue.Add(new KeyValuePair<string, int>($"{i + 1}.Hesap", alan));//Her elemanı KeyValuePair'ye eklerim.
+            }
+ 
+            return keyValue.OrderBy(e => e.Value).FirstOrDefault().Value;
+        }
+        private async Task<int> BirinciKutuHesapla(int[] birinciKutuBoyutlari)
+        {
+            if (birinciKutuBoyutlari.Count() > 0)
+            {
+                int yukseklik = birinciKutuBoyutlari.Max();
+                int taban = birinciKutuBoyutlari.Count();
+                //Console.WriteLine($"Birinci Kutu elemanı: {string.Join(",", birinciKutuBoyutlari).Pastel(Color.Green)} Alanı {(yukseklik * taban).ToString().Pastel(Color.AliceBlue)}");
+                return yukseklik * taban;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private async Task<int> IkinciKutuHesapla(int[] ikinciKutuBoyutlari)
+        {
+            if (ikinciKutuBoyutlari.Count() > 0)
+            {
+                //Console.WriteLine($"İkinci Kutu elemanları; {string.Join(",", ikinciKutuBoyutlari).Pastel(Color.DarkGreen)}");
+                int yukseklik = ikinciKutuBoyutlari.Max();
+                int taban = ikinciKutuBoyutlari.Count();
+                //Console.WriteLine($"İkinci Kutu elemanı: {string.Join(",", ikinciKutuBoyutlari).Pastel(Color.Green)} Alanı {(yukseklik * taban).ToString().Pastel(Color.AliceBlue)}");
+                return yukseklik * taban;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ KutuSorusu/Program.cs
@@ -0,0 +1,36 @@
+﻿using KutuSorusu;
+using Pastel;
+using System.Drawing;
+
+Kutu kutu = new Kutu();
+int sonuc = 0;
+
+int[] birinciSoru = { 3, 2, 4 };
+Console.WriteLine($"{"1.".Pastel(Color.Coral)} Sorudaki {string.Join(",", birinciSoru).Pastel(Color.Orange)} boyutları için gerekli alan hesaplanıyor.");
+sonuc = await kutu.AlaniniHesapla(birinciSoru);
+Console.WriteLine($"{"1.".Pastel(Color.Coral)} Soru İçin En Uygun Kullanilan Alan:{sonuc.ToString().Pastel(Color.FromArgb(255, 0, 0)).PastelBg("#34eb49")} \n");
+
+
+int[] ikinciSoru = { 5, 3, 2, 4 };
+Console.WriteLine($"{"2.".Pastel(Color.Aquamarine)} Sorudaki {string.Join(",", ikinciSoru).Pastel(Color.Orange)} boyutları için gerekli alan hesaplanıyor.");
+sonuc = await kutu.AlaniniHesapla(ikinciSoru);
+Console.WriteLine($"{"2.".Pastel(Color.Aquamarine)} Soru İçin En Uygun Kullanilan Alan:{sonuc.ToString().Pastel(Color.FromArgb(255, 0, 0)).PastelBg("#34eb49")} \n");
+
+int[] ucuncuSoru = { 5, 4, 5, 2, 1 };
+Console.WriteLine($"{"3.".Pastel(Color.MediumPurple)} Sorudaki {string.Join(",", ucuncuSoru).Pastel(Color.Orange)} boyutları için gerekli alan hesaplanıyor.");
+sonuc = await kutu.AlaniniHesapla(ucuncuSoru);
+Console.WriteLine($"{"3.".Pastel(Color.MediumPurple)} Soru İçin En Uygun Kullanilan Alan:{sonuc.ToString().Pastel(Color.FromArgb(255, 0, 0)).PastelBg("#34eb49")} \n");
+
+int[] dorduncuSoru = { 5, 5, 5, 2, 5, 5, 5 };
+Console.WriteLine($"{"4.".Pastel(Color.PeachPuff)} Sorudaki {string.Join(",", dorduncuSoru).Pastel(Color.Orange)} boyutları için gerekli alan hesaplanıyor.");
+sonuc = await kutu.AlaniniHesapla(dorduncuSoru);
+Console.WriteLine($"{"4.".Pastel(Color.PeachPuff)} Soru İçin En Uygun Kullanilan Alan:{sonuc.ToString().Pastel(Color.FromArgb(255, 0, 0)).PastelBg("#34eb49")} \n");
+
+int[] besinciSoru = { 1, 1, 6, 5, 5, 5 };
+Console.WriteLine($"{"5.".Pastel(Color.Plum)} Sorudaki {string.Join(",", besinciSoru).Pastel(Color.Orange)} boyutları için gerekli alan hesaplanıyor.");
+sonuc = await kutu.AlaniniHesapla(besinciSoru);
+Console.WriteLine($"{"5.".Pastel(Color.Plum)} Soru İçin En Uygun Kullanilan Alan:{sonuc.ToString().Pastel(Color.FromArgb(255, 0, 0)).PastelBg("#34eb49")} \n");
+
+
+Console.Write($"Uygulamadan çıkmak için {"<ENTER>".Pastel(Color.FromArgb(165, 229, 250))} tuşuna basın.");
+while (Console.ReadKey().Key != ConsoleKey.Enter) { }
